a71d4075711fd5076b1d83d111f7f881deb96221
sm_ayman <sultanmdayman@gmail.com>
2025-11-09 15:02:33 +0600

    All possible routes setup done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home/Home";
 import Root from "../layout/Root";
+import AllMovies from "../pages/AllMovies/AllMovies";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import MyCollection from "../pages/MyCollection/MyCollection";
+import MovieDetails from "../pages/MovieDetails/MovieDetails";
+import AddMovie from "../pages/AddMovie/AddMovie";
+import UpdateMovie from "../pages/UpdateMovie/UpdateMovie";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +17,34 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
+      },
+      {
+        path: "/movies",
+        element: <AllMovies></AllMovies>,
+      },
+      {
+        path: "/movies/:id",
+        element: <MovieDetails></MovieDetails>,
+      },
+      {
+        path: "/add",
+        element: <AddMovie></AddMovie>,
+      },
+      {
+        path: "/movies/update/:id",
+        element: <UpdateMovie></UpdateMovie>,
+      },
+      {
+        path: "/login",
+        elementL: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/my-collection",
+        element: <MyCollection></MyCollection>,
       },
     ],
   },
--- src/pages/AllMovies/AllMovies.jsx
+++ src/pages/AllMovies/AllMovies.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AllMovies = () => {
     return (
         <div>
-            
+            All Movies --
         </div>
     );
 };
